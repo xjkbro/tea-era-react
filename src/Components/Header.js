6aef46a2ee4fc8c9bdf543b9abb83c3f00ea7a1e
Jason-Kyle De Lara <delara@unlv.nevada.edu>
2020-12-03 15:02:38 -0800

    transfer

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,26 @@
 import React from "react";
+import {Jumbotron, Container, Row, Col} from 'reactstrap';
 
 function Header() {
-    return <h1>Header</h1>;
+    return (
+    <Jumbotron fluid className="fixed-top">
+        <Container className="mx-auto">
+            <Row>
+                <Col xs="4" className="align-self-center">
+                    <>TEA ERA </>
+                </Col>
+                <Col xs="2" className="align-self-center">
+    <i className="d-none d-md-block">home</i>
+                </Col>
+                <Col xs="2" className="align-self-center">
+                <i className="d-none d-md-block">home</i>
+                </Col>
+                <Col xs="2" className="align-self-center">
+                <i className="d-none d-md-block">home</i>
+                </Col>
+            </Row>
+        </Container>
+    </Jumbotron>);
 }
 
 export default Header;
