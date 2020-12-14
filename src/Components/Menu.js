d0b87e19744f08fe68f578438ad5875f699721b1
Jason-Kyle De Lara <jason8oh8@gmail.com>
2020-12-13 20:19:16 -0800

    menu

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,25 +1,45 @@
 import React from "react";
 import { menuItems } from "../shared/menuItems";
+import { Row, Col } from "reactstrap";
 
 function Menu() {
-    
+    //   let content = [];
+    //   let row = [];
+    //   menuItems.forEach((item, i) =>{
+    //     if((i+1) % 4 === 0){
+    //       content.push(
+    //         <div className="row" key={item.id}>
+    //           <article key={item.id} className="col-md-3">no</article>
+    //         </div>
+    //       )
+    //     }else{
+    //     row.push(<article key={item.id} className="col-md-3">yes</article>);
+    //     }
+    // });
 
-    let content = [];
-    let row = [];
-    menuItems.forEach((item, i) =>{
-      if((i+1) % 4 === 0){
-        content.push(
-          <div className="row" key={item.id}>       
-            <article key={item.id} className="col-md-3">no</article>
-          </div>
-        )
-      }else{
-      row.push(<article key={item.id} className="col-md-3">yes</article>);
-      }
-  });
-    return (<div className="container">
-        {content}
-    </div>);
+    const content = menuItems.map((item) => {
+        return (
+            <Col md="4" key={item.id}>
+                <img
+                    className="w-100 img-thumb gallery-img"
+                    src={item.src}
+                    alt={item.altText}
+                />
+                <div className="overlay text-center pt-100">
+                    <h4 className="text-white">{item.itemName}</h4>
+                    <div className="text-white">$6.50</div>
+                    <p className="text-white">{item.desc}</p>
+                    <button className="btn btn-primary">Add to Cart</button>
+                </div>
+            </Col>
+        );
+    });
+
+    return (
+        <div className="container">
+            <Row className="py-4">{content}</Row>
+        </div>
+    );
 }
 
 export default Menu;
